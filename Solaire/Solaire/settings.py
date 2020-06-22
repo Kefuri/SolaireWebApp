cb677806b08053e240e73f5af9c669d0ad753a68
Haydon Houghton <haydonhoughton@outlook.com>
2020-06-22 10:30:05 +0100

    Setup view imports for regiser page

--- Solaire/Solaire/settings.py
+++ Solaire/Solaire/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'battles.apps.BattlesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
